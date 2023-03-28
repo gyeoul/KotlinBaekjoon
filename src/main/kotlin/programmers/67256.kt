beedc2ec6c004b1d13b21406fae15e053422546d
gyeoul <clsmay.1st@gmail.com>
2023-03-28 18:51:26 +0900

    Programmers 67256 Solved

---
+++ src/main/kotlin/programmers/67256.kt
@@ -0,0 +1,56 @@
+package programmers
+
+import kotlin.math.abs
+
+class Solution {
+    private var leftNow = arrayOf(3, 0)
+    private var rightNow = arrayOf(3, 2)
+    private val answer = StringBuilder()
+    fun findHand(idx: Array<Int>, hand: Boolean) {
+        if (hand) {
+            rightNow = idx
+            answer.append("R")
+        } else {
+            leftNow = idx
+            answer.append("L")
+        }
+    }
+
+    fun solution(numbers: IntArray, hand: String): String {
+        val isRight: Boolean = hand == "right"
+        var idx = 0
+        var now: Int
+        while (idx < numbers.size) {
+            now = numbers[idx]
+            if (now == 0) now += 11
+            when (now % 3) {
+                0 -> {
+                    findHand(arrayOf((now / 3) - 1, 2), true)
+                }
+
+                1 -> {
+                    findHand(arrayOf(now / 3, 0), false)
+                }
+
+                2 -> {
+                    if ((abs(now / 3 - rightNow[0]) + (rightNow[1] + 1) % 2) == (abs(now / 3 - leftNow[0]) + (leftNow[1] + 1) % 2)) {
+                        findHand(arrayOf(now / 3, 1), isRight)
+                    } else {
+                        findHand(
+                            arrayOf(now / 3, 1),
+                            (abs(now / 3 - rightNow[0]) + (rightNow[1] + 1) % 2) < (abs(now / 3 - leftNow[0]) + (leftNow[1] + 1) % 2)
+                        )
+                    }
+                }
+            }
+            idx++
+        }
+        return answer.toString()
+    }
+}
+
+
+fun main() {
+    val s = Solution()
+    println(s.solution(intArrayOf(1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5), "right"))
+}
