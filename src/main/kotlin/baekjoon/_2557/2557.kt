64e71de130f4345f3476f28b5ecca780b2ffe3ea
gyeoul <clsmay.1st@gmail.com>
2023-04-02 02:05:00 +0900

    B5 2557

---
+++ src/main/kotlin/baekjoon/_2557/2557.kt
@@ -0,0 +1,11 @@
+package baekjoon._2557
+
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
+fun main() {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    bw.write("Hello World!")
+    bw.flush()
+    bw.close()
+}
